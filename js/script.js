1a3916e1193ac9c6207458aff583c0bd432a7b09
bruno.falcao <brunocesar.sf.16@gmail.com>
2025-11-16 18:50:58 -0400

    Added draggin functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("print: hello, worl")
+document.querySelectorAll('.kanban-card').forEach(card => {
+    card.addEventListener('dragstart', e => {
+        e.currentTarget.classList.add('dragging');
+    });
+
+    card.addEventListener('dragend', e => {
+        e.currentTarget.classList.remove('dragging');
+    });
+});
+
+document.querySelectorAll('.kanban-cards').forEach(column => {
+    column.addEventListener('dragover', e => {
+        e.preventDefault();
+
+        const dragging = document.querySelector('.dragging');
+        column.appendChild(dragging);
+    });
+
+    column.addEventListener('drop', e => {
+        const dragging = document.querySelector('.dragging');
+        column.appendChild(dragging);
+    });
+
+});
